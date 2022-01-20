ed3082bd1c7d11b3747468212de88b3d0e19fca9
Nikita Ryzhkin <timongo@bk.ru>
2022-01-20 19:30:58 +0400

    Initial Commit

---
+++ Light Changer/Light_ChangerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Light_ChangerApp.swift
+//  Light Changer
+//
+//  Created by Никита Рыжкин on 20.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct Light_ChangerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
